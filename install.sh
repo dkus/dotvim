4e4b3c3c725149482927878f69c4006cce7b46d8
Davor Kus <kusdavor@gmail.com>
2021-02-02 18:33:37 +0100

    Add install.sh and fix fzf install

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# thanks to https://github.com/maralla/dotvim/blob/master/install.sh
+
+log() {
+    echo -e "\033[1m\033[32mINFO:\033[0m $1"
+}
+
+if [ -d "$HOME/.vim" ]
+then
+    ts=$(date +"%Y%m%d@%H:%M:%S")
+    log "Make backup of '\$HOME/.vim' to '\$HOME/.vim.$ts'"
+    mv $HOME/{.vim,.vim.$ts}
+fi
+
+p="$HOME/.local/bin/pack"
+log "Installing pack plugin for vim into ~/.local/bin/pack"
+(mkdir -p $p && cd $p)
+v=v0.1.0
+os=x86_64-unknown-linux-gnu
+wget -q https://github.com/maralla/pack/releases/download/$v/pack-$v-$os.tar.gz -O $p/pack-$v.tar.gz
+tar -vxf pack-$v-$os.tar.gz
+
+case :$PATH: in
+  *:$p:*)  ;;  # do nothing
+  *) PATH=$p:$PATH ;;
+esac
+log "Done installing pack"
+
+log "Cloning dkus/dotvim..."
+git clone https://github.com/dkus/dotvim.git $HOME/.vim
+log "Install vim packs..."
+(cd $HOME/.vim && pack install)
+log "Done"
